995f37aa9fd4cf511d44b308fab3fea2eaa72d22
skrulling <mikkeleb@gmail.com>
2023-08-02 21:52:19 +0200

    pomodoro mode

--- README.md
+++ README.md
@@ -5,4 +5,29 @@ Take more breaks, buddy 🔥
 
 ## Getting started
 
-After installing the extension, open the command palette `ctrl + shift + p` and run the command `Start BreakBuddy`
+After installing the extension, open the command palette `ctrl + shift + p` and run the command `BreakBuddy: Start `
+
+## 🔥🔥 BreakBuddy Pomodoro Mode 🍅🔥🔥
+
+Ready to level up your productivity, bro? BreakBuddy's got you covered with our **Pomodoro Mode**! 
+
+So, what's this Pomodoro thing all about? It's simple, dude. It's a productivity hack that's going to get you in the zone. 🧠💡 
+
+Here's how it works:
+
+1. You grind for **25 minutes**. No distractions, just pure hustle. 
+
+2. When your time's up, BreakBuddy taps you on the shoulder and says "Hey bro, take 5!". This is your **short break**. Grab a drink, do some stretches, check out what's happening in the world, or whatever chills you out. 🏝️
+
+3. Rinse and repeat three times. On the fourth time, BreakBuddy says "Hey bro, take a longer break, you've earned it". Now you get a **15-minute break** to recharge. 🚀
+
+4. After your long break, you start over. Simple, right?
+
+To start the Pomodoro Mode, just run the `breakbuddy.pomodoroStart` command in the command palette. 
+
+- Press `Ctrl+Shift+P` to open the command palette
+- Search for "BreakBuddy: Start Pomodoro Mode"
+
+That's it, bro! Now get back to that grind and let BreakBuddy handle your break schedule.
+
+Remember, staying productive isn't about working non-stop. It's about working smart. And taking regular breaks is part of working smart. So let's get those gains, bro! 💪💪💪
--- src/extension.ts
+++ src/extension.ts
@@ -1,55 +1,127 @@
 import * as vscode from 'vscode';
 
 let timer: NodeJS.Timeout | null = null;
+let pomodoroCount = 0;
+let statusBar: vscode.StatusBarItem;
+
 
 export function activate(context: vscode.ExtensionContext) {
 	console.log('Starting breakbuddy');
+	statusBar = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
+    context.subscriptions.push(statusBar);
 
 	// If a timer was previously set, and the end-time has not passed, start the timer
 	const timerEnd = context.globalState.get<number>('timerEnd');
+	const mode = context.globalState.get<string>('mode');
 	if (timerEnd && Date.now() < timerEnd) {
-		startTimer(context, timerEnd - Date.now());
+		if (mode === 'pomodoro') {
+			startPomodoroTimer(context, timerEnd - Date.now());
+		} else {
+			startTimer(context, timerEnd - Date.now());
+		}
 	}
 
 	let disposable = vscode.commands.registerCommand('breakbuddy.start', () => {
 		vscode.window.showInformationMessage('Get programming bro. I\'ll remind you to take a break in 50 minutes 🔥');
-		startTimer(context, 3000000); // 50 minutes
+		context.globalState.update('mode', 'normal');
+		// startTimer(context, 3000000); // 50 minutes
+		startTimer(context, 2000); // 50 minutes
 	});
 
 	let stop = vscode.commands.registerCommand('breakbuddy.stop', () => {
 		stopTimer(context);
 	});
 
+	let pomodoro = vscode.commands.registerCommand('breakbuddy.pomodoroStart', () => {
+		vscode.window.showInformationMessage('Pomodoro Mode! Let\'s get to work! 🔥\n Next break in 25 minutes');
+		context.globalState.update('mode', 'pomodoro');
+		pomodoroCount = 0;
+		// startTimer(context, 1500000, false); // 25 minutes
+		startPomodoroTimer(context, 3000, false); // 25 minutes
+	});
+
+
 	context.subscriptions.push(disposable);
 	context.subscriptions.push(stop);
+	context.subscriptions.push(pomodoro);
 }
 
 function startTimer(context: vscode.ExtensionContext, duration: number) {
-	stopTimer(context);
+	stopTimer(context, false); // Don't show the "Stopping breakbuddy" message when starting a new timer
 	context.globalState.update('timerEnd', Date.now() + duration);
 	timer = setTimeout(() => takeBreak(context), duration);
 }
 
+// This will be a new function dedicated to Pomodoro mode
+function startPomodoroTimer(context: vscode.ExtensionContext, duration: number, isBreak: boolean = false) {
+    stopTimer(context, false);
+    context.globalState.update('timerEnd', Date.now() + duration);
+    if(isBreak) {
+        const timeLeft = duration;
+        const minutes = Math.floor(timeLeft / 60000);
+        const seconds = Math.floor((timeLeft % 60000) / 1000); 
+        statusBar.text = `BreakBuddy: ${minutes}:${seconds < 10 ? '0' : ''}${seconds}`; // Initial countdown display
+        statusBar.show();
+        timer = setInterval(() => {
+            const timeLeft = (context.globalState.get<number>('timerEnd') as number) - Date.now();
+            if (timeLeft <= 0) {
+                statusBar.hide();
+                clearInterval(timer as NodeJS.Timeout);
+                takePomodoroBreak(context, isBreak);
+            } else {
+                const minutes = Math.floor(timeLeft / 60000);
+                const seconds = Math.floor((timeLeft % 60000) / 1000); 
+                statusBar.text = `BreakBuddy: ${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+            }
+        }, 1000);
+    } else {
+        timer = setTimeout(() => takePomodoroBreak(context, isBreak), duration);
+    }
+}
+
+// This function will handle the breaks in Pomodoro mode
+async function takePomodoroBreak(context: vscode.ExtensionContext, isBreak: boolean) {
+    if (!isBreak) {
+        pomodoroCount++;
+        let message = 'Time to take a ';
+        message += pomodoroCount % 4 === 0 ? 'long break 🏖️' : 'short break 🙌';
+        vscode.window.showInformationMessage(message);
+        // startPomodoroTimer(context, pomodoroCount % 4 === 0 ? 900000 : 300000, true); // 15 minutes and 5 minutes
+        startPomodoroTimer(context, pomodoroCount % 4 === 0 ? 9000 : 3000, true); // 15 minutes and 5 minutes
+    } else {
+        const response = await vscode.window.showQuickPick(['$(debug-start) Back to work 👌', '$(debug-stop) Done for today 🥱'], {title: 'BreakBuddy 🔥'});
+        if(response === '$(debug-start) Back to work 👌') {
+            vscode.window.showInformationMessage('Nice! See you in 25 minutes 😎');
+            // startPomodoroTimer(context, 1500000, false); // 25 minutes
+            startPomodoroTimer(context, 1500, false); // 25 minutes
+        } else {
+            stopTimer(context);
+        }
+    }
+}
+
 async function takeBreak(context: vscode.ExtensionContext) {
-	vscode.window.showInformationMessage('Time to take a break there buddy 🙌 \nI suggest taking 10 minutes');
-	const response = await vscode.window.showQuickPick(['$(debug-start) Back from my break 👌', '$(debug-stop) Done for today 🥱'], {title: 'BreakBuddy 🔥'});
-	
-	if(response === '$(debug-start) Back from my break 👌') {
+	// Break time is over
+	const response = await vscode.window.showQuickPick(['$(debug-start) Back to work 👌', '$(debug-stop) Done for today 🥱'], { title: 'BreakBuddy 🔥' });
+	if (response === '$(debug-start) Back to work 👌') {
 		vscode.window.showInformationMessage('Nice! See you in 50 minutes 😎');
-		startTimer(context, 3000000); // 50 minutes
+		startTimer(context, 2000); // 50 minutes
 	} else {
 		stopTimer(context);
 	}
 }
 
-function stopTimer(context: vscode.ExtensionContext) {
+function stopTimer(context: vscode.ExtensionContext, showMessage: boolean = true) {
 	if (timer) {
 		clearTimeout(timer);
 		timer = null;
 		context.globalState.update('timerEnd', undefined);
-		vscode.window.showInformationMessage('Stopping breakbuddy');
+		if (showMessage) {
+			vscode.window.showInformationMessage('Nice work today buddster 💎 Stopping breakbuddy..');
+		}
 	}
 }
+
 
 export function deactivate() {
 	// Don't need to call stopTimer here. When deactivating, VS Code will automatically stop all running timers.
